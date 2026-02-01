53281fcb102392742596c9250cf5a47fd18a6ef8
komed3 <pk97@outlook.de>
2026-02-01 14:02:24 +0100

    setup socket and device services

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,22 @@
 import { createServer } from 'node:http';
 import express from 'express';
+import { Server } from 'socket.io';
+import { DeviceService } from './services/DeviceSession';
 
 
 const app = express();
 const httpServer = createServer( app );
+
+const io = new Server( httpServer, {
+    cors: {
+        origin: '*',
+        methods: [ 'GET', 'POST', 'PUT', 'DELETE' ]
+    },
+    maxHttpBufferSize: 5e7,
+    pingTimeout: 600000,
+    pingInterval: 25000,
+    connectTimeout: 600000
+} );
+
+const deviceService = DeviceService.getInstance();
+const PORT = process.env.PORT || 3001;
