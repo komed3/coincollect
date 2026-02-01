782384b7bd23fe1dfe47fe4ef1cfb50498dfb0d1
komed3 <pk97@outlook.de>
2026-02-01 10:13:14 +0100

    describe coin interface

---
+++ shared/types.ts
@@ -0,0 +1,55 @@
+export interface Coin {
+    id: string;
+    name: string;
+
+    type: any;
+    originCountry: string;
+    series?: string;
+    tags?: string[];
+
+    mintYear?: number;
+    mintage?: number;
+    mintMark?: string;
+
+    faceValue?: {
+        amount: number;
+        currency: string;
+    };
+
+    grade: any;
+
+    shape: any;
+    material?: any;
+    dimensions?: {
+        diameter?: number;
+        thickness?: number;
+        weight?: number;
+    };
+
+    description?: string;
+    design?: {
+        obverse?: string;
+        reverse?: string;
+        edge?: string;
+    };
+
+    purchaseInfo?: {
+        price: number;
+        currency: string;
+        date?: string;
+    };
+    collectorValue: {
+        amount: number;
+        currency: string;
+        lastUpdated?: string;
+    };
+
+    images?: {
+        obverse?: string;
+        reverse?: string;
+        other?: string[];
+    };
+
+    createdAt: string;
+    updatedAt: string; 
+}
