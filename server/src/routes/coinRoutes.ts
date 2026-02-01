a275543e826733f04e34160803556be1210ca2f7
komed3 <pk97@outlook.de>
2026-02-01 13:57:53 +0100

    define coin routes

---
+++ server/src/routes/coinRoutes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { CoinController } from '../controllers/CoinController';
+import { ImageController } from '../controllers/ImageController';
+
+const router = Router();
+const coinController = new CoinController();
+const imageController = new ImageController();
+
+router.get( '/', coinController.getAllCoins );
+router.get( '/:id', coinController.getCoin );
+router.post( '/', coinController.createCoin );
+router.put( '/:id', coinController.updateCoin );
+router.delete( '/', coinController.deleteAllCoins );
+router.delete( '/:id', coinController.deleteCoin );
+
+router.post( '/upload', imageController.uploadImage );
+
+export default router;
