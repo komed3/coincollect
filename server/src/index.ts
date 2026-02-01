4d741abd09905f6e932502895548912a27dc993b
komed3 <pk97@outlook.de>
2026-02-01 14:06:53 +0100

    register routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
 import { createServer } from 'node:http';
+import { networkInterfaces } from 'node:os';
 import { join } from 'node:path';
 import cors from 'cors';
-import express from 'express';
+import express, { type Request, type Response } from 'express';
 import { Server } from 'socket.io';
+import coinRoutes from './routes/coinRoutes';
+import settingsRoutes from './routes/settingsRoutes';
 import { DeviceService } from './services/DeviceSession';
 
 
@@ -23,7 +26,36 @@ const io = new Server( httpServer, {
 const deviceService = DeviceService.getInstance();
 const PORT = process.env.PORT || 3001;
 
+// Middleware
 app.use( cors() );
 app.use( express.urlencoded( { limit: '50mb', extended: true } ) );
 app.use( express.json( { limit: '50mb' } ) );
 app.use( '/images', express.static( join( __dirname, '../data/images' ) ) );
+
+// Routes
+app.use( '/api/coins', coinRoutes );
+app.use( '/api/settings', settingsRoutes );
+
+app.get( '/api/status', ( _: Request, res: Response ) => {
+    res.json( { status: 'online', version: '0.1.0-alpha' } );
+} );
+
+app.get( '/api/ip', ( _: Request, res: Response ) => {
+    const interfaces = networkInterfaces();
+    let ip = 'localhost';
+
+    for ( const devName in interfaces ) {
+        const iface = interfaces[ devName ];
+
+        for ( let i = 0; i < ( iface ?? [] ).length; i++ ) {
+            const alias = iface![ i ];
+
+            if ( alias.family === 'IPv4' && alias.address !== '127.0.0.1' && ! alias.internal ) {
+                ip = alias.address;
+                break;
+            }
+        }
+    }
+
+    res.json( { ip } );
+} );
