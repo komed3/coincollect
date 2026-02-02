d7d7c557edd6d5241ff9023a3d852367ee7de229
komed3 <pk97@outlook.de>
2026-02-02 19:31:55 +0100

    prep. db class

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,11 +1,21 @@
+import { join } from 'node:path';
+import { Low } from 'lowdb';
 import { JSONFile } from 'lowdb/node';
-import { Coin, Database } from '../types';
+import { Database } from '../types';
 
 export class DatabaseService {
 
     private static instance: DatabaseService;
 
-    private constructor () {}
+    private dbFile: string;
+    private adapter: JSONFile< Database > | undefined;
+    private db: Low< Database > | undefined;
+    private writeTimer: NodeJS.Timeout | undefined;
+    private writeDelay = 150;
+
+    private constructor () {
+        this.dbFile = join( __dirname, '../../db/db.json' );
+    }
 
     public static getInstance () : DatabaseService {
         return DatabaseService.instance ||= new DatabaseService();
