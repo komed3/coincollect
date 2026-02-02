850a2726bc2668bcb5f767cbb6811e32fcfcc392
komed3 <pk97@outlook.de>
2026-02-02 17:31:34 +0100

    Updata types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,34 +1,3 @@
-export enum CoinType {
-    Circulation = 'circulation',
-    Commemorative = 'Commemorative',
-    Bullion = 'bullion',
-    Medal = 'medal',
-    Pattern = 'pattern',
-    Ingot = 'ingot',
-    Stamp = 'stamp',
-    Other = 'other'
-}
-
-export enum CoinShape {
-    Round = 'round',
-    Oval = 'oval',
-    Square = 'square',
-    Polygonal = 'polygonal',
-    Irregular = 'irregular',
-    Other = 'other'
-}
-
-export enum CoinGrade {
-    G = 'G',
-    VG = 'VG',
-    F = 'F',
-    VF = 'VF',
-    XF = 'XF',
-    UNC = 'UNC',
-    BU = 'BU',
-    FDC = 'FDC'
-}
-
 export interface Coin {
     id: string;
     name: string;
@@ -52,7 +21,58 @@ export interface Coin {
         currency?: string;
     };
 
+    description?: string;
     design?: {
         shape?: CoinShape;
+        obverse?: string;
+        reverse?: string;
+        edge?: string;
     };
+
+    dimensions?: {
+        diameter?: number;
+        thickness?: number;
+        weight?: number;
+    };
+
+    material?: CoinMaterial[];
+
+    createdAt: string;
+    updatedAt: string;
+}
+
+export enum CoinType {
+    Circulation = 'circulation',
+    Commemorative = 'Commemorative',
+    Bullion = 'bullion',
+    Medal = 'medal',
+    Pattern = 'pattern',
+    Ingot = 'ingot',
+    Other = 'other'
+}
+
+export enum CoinGrade {
+    G = 'G',
+    VG = 'VG',
+    F = 'F',
+    VF = 'VF',
+    XF = 'XF',
+    UNC = 'UNC',
+    BU = 'BU',
+    FDC = 'FDC'
+}
+
+export enum CoinShape {
+    Round = 'round',
+    Oval = 'oval',
+    Square = 'square',
+    Polygonal = 'polygonal',
+    Irregular = 'irregular',
+    Other = 'other'
+}
+
+export interface CoinMaterial {
+    name: string;
+    fineness?: number;
+    portion?: number;
 }
