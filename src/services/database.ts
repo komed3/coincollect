4e1f1ea1ffb668a5daa630d755396bec9c5790ea
komed3 <pk97@outlook.de>
2026-02-02 18:00:19 +0100

    create db service class

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,1 +1,14 @@
 import { JSONFile } from 'lowdb/node';
+import { Coin, Database } from '../types';
+
+export class DatabaseService {
+
+    private static instance: DatabaseService;
+
+    private constructor () {}
+
+    public static getInstance () : DatabaseService {
+        return DatabaseService.instance ||= new DatabaseService();
+    }
+
+}
