8035cfbabf7208c57a1b84c71479ca7965161257
komed3 <pk97@outlook.de>
2026-02-03 19:35:12 +0100

    fetch coin

--- src/pages/Coin.ts
+++ src/pages/Coin.ts
@@ -1,5 +1,10 @@
 import type { Request, Response } from 'express';
+import DB from '../services/DatabaseService';
 
 export const coin = async ( req: Request, res: Response ) : Promise< void > => {
-    res.render( 'coin', { title: req.t( 'coin.title' ) } );
+    const { id } = req.params;
+    const coin = await DB.getCoinById( id as string );
+
+    if ( ! coin ) res.redirect( '/404' );
+    else res.render( 'coin', { title: req.t( 'coin.title', { name: coin.name } ), coin } );
 };
--- src/routes/AppRoutes.ts
+++ src/routes/AppRoutes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { coin } from '../pages/Coin';
 import { editor } from '../pages/Editor';
 import { home } from '../pages/Home';
 import { settings } from '../pages/Settings';
@@ -7,6 +8,7 @@ import { stats } from '../pages/Stats';
 const appRoutes = Router();
 
 appRoutes.get( '/', home );
+appRoutes.get( '/coin/:id', coin );
 appRoutes.get( '/add', editor );
 appRoutes.get( '/settings', settings );
 appRoutes.get( '/stats', stats );
