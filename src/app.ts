3564a959b718d802db36360072864020cfe13d23
komed3 <pk97@outlook.de>
2026-02-03 10:44:27 +0100

    register middleware + routes

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,14 @@
 import express from 'express';
+import { apiRoutes } from './routes/APIRoutes';
 
+const PORT = process.env.PORT || 3001;
 const app = express();
-app.listen( 3000 );
+
+// Middleware
+app.use( express.urlencoded( { limit: '50mb', extended: true } ) );
+app.use( express.json( { limit: '50mb' } ) );
+
+// Routes
+app.use( '/api', apiRoutes );
+
+app.listen( PORT );
