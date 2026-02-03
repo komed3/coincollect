50394d8109e5112345fff9ac2e4a9ff2358a9469
komed3 <pk97@outlook.de>
2026-02-03 17:26:22 +0100

    basic html structure

--- src/pages/Home.ts
+++ src/pages/Home.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from 'express';
 
 export const home = async ( req: Request, res: Response ) : Promise< void > => {
-    res.render( 'home' );
+    res.render( 'home', { title: req.t( 'home.title' ) } );
 };
