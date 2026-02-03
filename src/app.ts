77f6822f5662313cdd0c02789afef88a5343158d
komed3 <pk97@outlook.de>
2026-02-03 16:59:24 +0100

    set up view engine

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
+import { join } from 'node:path';
 import express from 'express';
 import { apiRoutes } from './routes/APIRoutes';
+import { I18nService } from './services/i18nService';
 
-const PORT = process.env.PORT || 3001;
+// Express app
 const app = express();
+
+// Set up view engine
+app.set( 'views', join( process.cwd(), 'views' ) );
+app.set( 'view engine', 'pug' );
 
 // Middleware
 app.use( express.urlencoded( { limit: '50mb', extended: true } ) );
 app.use( express.json( { limit: '50mb' } ) );
+app.use( I18nService );
 
-// Routes
+// Mount routes
 app.use( '/api', apiRoutes );
 
+// Listen ...
+const PORT = process.env.PORT || 3001;
 app.listen( PORT );
