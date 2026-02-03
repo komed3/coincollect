027dcbe9b70aa5051f94794267d9ad9ca8958737
komed3 <pk97@outlook.de>
2026-02-03 16:57:40 +0100

    create i18next handler

--- src/services/i18nService.ts
+++ src/services/i18nService.ts
@@ -1,2 +1,28 @@
+import { join } from 'node:path';
 import i18next from 'i18next';
-import Backend from 'i18next-fs-backend';
+import I18NexFsBackend from 'i18next-fs-backend';
+import { LanguageDetector, handle } from 'i18next-http-middleware';
+
+await i18next.use( I18NexFsBackend ).use( LanguageDetector ).init( {
+    fallbackLng: 'en-US',
+    preload: [ 'en-US', 'de-DE' ],
+    supportedLngs: [ 'en-US', 'de-DE' ],
+    cleanCode: true,
+    saveMissing: false,
+    debug: false,
+    backend: {
+        loadPath: join( process.cwd(), 'locales/{{lng}}.json' ),
+    },
+    detection: {
+        order: [ 'cookie', 'header' ],
+        lookupCookie: 'locale',
+        caches: [ 'cookie' ],
+        cookieSameSite: 'strict',
+        cookieSecure: false
+    },
+    interpolation: {
+        escapeValue: false
+    }
+} );
+
+export const I18nService = handle( i18next );
