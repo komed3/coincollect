1bcefd8fc5b1708c8152530547dc7d4372289d06
komed3 <pk97@outlook.de>
2026-02-03 09:59:15 +0100

    get db meta data

--- src/controllers/CoinController.ts
+++ src/controllers/CoinController.ts
@@ -7,14 +7,25 @@ export class CoinController {
 
     constructor () {}
 
+    private async catch ( res: Response, msg: string, fn: () => Promise< void > ) : Promise< void > {
+        try { await fn() } catch ( err ) { res.status( 500 ).json( { msg, err } ) }
+    }
+
+    public async getMeta ( _:Request, res: Response ) : Promise< void > {
+        await this.catch( res, 'Failed to fetch meta', async () => {
+            res.json( await this.dbService.getMetaData() );
+        } );
+    }
+
     public async getAllCoins ( _: Request, res: Response ) : Promise< void > {
-        try { res.json( await this.dbService.getAllCoins() ) }
-        catch ( error ) { res.status( 500 ).json( { msg: 'Failed to fetch coins', error } ) }
+        await this.catch( res, 'Failed to fetch coins', async () => {
+            res.json( await this.dbService.getAllCoins() );
+        } );
     }
 
     public async searchCatalog ( req: Request, res: Response ) : Promise< void > {
-        try {
-            const query = {
+        await this.catch( res, 'Failed to search catalog', async () => {
+            res.json( await this.dbService.searchCatalog( {
                 text: req.query.text ? req.query.text as string : undefined,
                 filters: ( req.query.filters ?? {} ) as Record< string, any >,
                 range: ( req.query.range ?? {} ) as Record< string, { min?: number; max?: number } >,
@@ -22,25 +33,18 @@ export class CoinController {
                     limit: req.query.limit ? parseInt( req.query.limit as string ) : undefined,
                     offset: req.query.offset ? parseInt( req.query.offset as string ) : undefined
                 }
-            };
-
-            res.json( await this.dbService.searchCatalog( query ) );
-        }
-        catch ( error ) {
-            res.status( 500 ).json( { msg: 'Failed to fetch coins', error } );
-        }
+            } ) );
+        } );
     }
 
     public async getCoin ( req: Request, res: Response ) : Promise< void > {
-        try {
+        await this.catch( res, 'Failed to fetch coin by id', async () => {
             const { id } = req.params;
             const coin = await this.dbService.getCoinById( id as string );
 
             if ( coin ) res.json( coin );
-            else res.status( 404 ).json( { error: 'Coin not found' } );
-        } catch ( error ) {
-            res.status( 500 ).json( { msg: 'Failed to fetch coin', error } );
-        }
+            else res.status( 404 ).json( { msg: 'Coin not found', id } );
+        } );
     }
 
 }
--- src/routes/APIRoutes.ts
+++ src/routes/APIRoutes.ts
@@ -4,6 +4,7 @@ import { CoinController } from '../controllers/CoinController';
 const coins = Router();
 const coinController = new CoinController();
 
+coins.get( '/meta', coinController.getMeta );
 coins.get( '/all', coinController.getAllCoins );
 coins.get( '/search', coinController.searchCatalog );
 coins.get( '/get/:id', coinController.getCoin );
