c0cd57f52c68ad52ddeff10d993819391e1a62b8
komed3 <pk97@outlook.de>
2026-02-04 11:41:12 +0100

    implement value chart

--- public/js/chart.js
+++ public/js/chart.js
@@ -5,12 +5,14 @@ class CCChart {
     constructor () {
         Chart.defaults.responsive = true;
         Chart.defaults.maintainAspectRatio = false;
+        Chart.defaults.interaction.intersect = false;
+        Chart.defaults.interaction.mode = 'index';
         Chart.defaults.offset = true;
         Chart.defaults.clip = false;
         Chart.defaults.layout.padding = 6;
 
         Chart.defaults.font.family = '"Inter", sans-serif';
-        Chart.defaults.font.size = 15;
+        Chart.defaults.font.size = 12;
         Chart.defaults.font.weight = 400;
 
         Chart.defaults.backgroundColor = '#fff';
@@ -34,9 +36,9 @@ class CCChart {
         Chart.defaults.plugins.legend.labels.boxWidth = 16;
         Chart.defaults.plugins.legend.labels.boxHeight = 16;
 
-        Chart.defaults.plugins.tooltip.titleColor = '#000';
+        Chart.defaults.plugins.tooltip.titleColor = '#777';
         Chart.defaults.plugins.tooltip.bodyColor = '#000';
-        Chart.defaults.plugins.tooltip.footerColor = '#000';
+        Chart.defaults.plugins.tooltip.footerColor = '#777';
         Chart.defaults.plugins.tooltip.backgroundColor = '#fff';
         Chart.defaults.plugins.tooltip.borderColor = '#e5e5e5';
         Chart.defaults.plugins.tooltip.borderWidth = 1;
@@ -50,13 +52,14 @@ class CCChart {
 
         Chart.defaults.plugins.tooltip.titleFont = {
             family: Chart.defaults.font.family,
-            size: Chart.defaults.font.size,
-            weight: 700
+            size: 11,
+            weight: 500
         };
 
         Chart.defaults.plugins.tooltip.bodyFont = {
-            size: 26,
-            weight: 700
+            family: Chart.defaults.font.family,
+            size: 14,
+            weight: 300
         };
 
         Chart.defaults.plugins.tooltip.footerFont = {
@@ -82,7 +85,63 @@ class CCChart {
     }
 
     renderValueChart ( uuid, data, ctx ) {
-        const chart = new Chart( ctx, {} );
+        const omv = ( data.omv ?? [] ).map( o => ( { x: o.date, y: o.value } ) );
+        if ( data.purchase?.date ) omv.push( { x: data.purchase.date, y: data.purchase.value } );
+        const th = data.purchase ? omv.map( o => ( { x: o.x, y: data.purchase.value } ) ) : [];
+
+        const chart = new Chart( ctx, {
+            type: 'line',
+            data: {
+                datasets: [ {
+                    label: 'OMV',
+                    data: omv,
+                    borderWidth: 2,
+                    borderColor: '#c5851f',
+                    hoverBorderWidth: 2,
+                    hoverBorderColor: '#c5851f',
+                    pointRadius: 5,
+                    pointBackgroundColor: '#fff',
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: '#fff'
+                }, {
+                    label: 'Purchase',
+                    data: th,
+                    borderWidth: 0,
+                    hoverBorderWidth: 0,
+                    fill: true,
+                    backgroundColor: '#c5321f33',
+                    pointRadius: 0,
+                    pointHoverRadius: 0
+                } ]
+            },
+            options: {
+                plugins: {
+                    tooltip: {
+                        filter ( item ) {
+                            return item.datasetIndex !== 1;
+                        }
+                    }
+                },
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'year',
+                            tooltipFormat: 'PP'
+                        },
+                        grid: {
+                            drawOnChartArea: false,
+                            tickLength: 6
+                        }
+                    },
+                    y: {
+                        beginAtZero: true,
+                        border: { dash: [ 5, 5 ] },
+                        ticks: { maxTicksLimit: 4 }
+                    }
+                }
+            }
+        } );
 
         this.charts.set( uuid, chart );
         return chart;
