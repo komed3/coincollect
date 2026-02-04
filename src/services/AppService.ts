db4fffc4d44a2410c7ec7e87f2b170d9fc2756a1
komed3 <pk97@outlook.de>
2026-02-05 00:02:33 +0100

    expose translations

--- src/services/AppService.ts
+++ src/services/AppService.ts
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 export const appService = ( req: Request, res: Response, next: NextFunction ) : void => {
     res.locals.lang = req.language;
     res.locals.supportedLngs = req.languages;
+    res.locals.translations = req.t( '_', { returnObjects: true } );
     res.locals.uuid = uuidv4;
     next();
 };
