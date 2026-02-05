41b119991c0d5d83f9f1c4e898e0f4efbf22f2a5
komed3 <pk97@outlook.de>
2026-02-05 23:16:14 +0100

    set currency

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,6 +1,14 @@
 document.addEventListener( 'DOMContentLoaded', function () {
     document.querySelector( '#languageSelect' ).addEventListener( 'change', ( e ) => {
-        document.cookie = `locale=${e.target.value}; path=/; max-age=${ 60 * 60 * 24 * 365 }`;
+        document.cookie = `locale=${ e.target.value }; path=/; max-age=${ 60 * 60 * 24 * 365 }`;
         setTimeout( () => window.location.reload(), 250 );
+    } );
+
+    document.querySelector( '#currencySelect' ).addEventListener( 'change', async ( e ) => {
+        await fetch( '/api/currency', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify( { currency: e.target.value } )
+        } );
     } );
 } );
