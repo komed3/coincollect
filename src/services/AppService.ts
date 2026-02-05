dae27a1fa74794c708f80b97edc83591aca1623e
komed3 <pk97@outlook.de>
2026-02-05 16:09:55 +0100

    expose db currency

--- src/services/AppService.ts
+++ src/services/AppService.ts
@@ -1,8 +1,10 @@
 import type { NextFunction, Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import DB from './DatabaseService';
 
-export const appService = ( req: Request, res: Response, next: NextFunction ) : void => {
+export const appService = async ( req: Request, res: Response, next: NextFunction ) : Promise< void > => {
     res.locals.lang = req.language;
+    res.locals.currency = await DB.getCurrency();
     res.locals.supportedLngs = req.languages;
     res.locals.translations = req.t( '_', { returnObjects: true } );
     res.locals.uuid = uuidv4;
