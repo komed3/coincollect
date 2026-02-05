6af270d618e6dae97bf33bd5e736dd8af82818e0
komed3 <pk97@outlook.de>
2026-02-05 20:00:47 +0100

    add api routes

--- src/routes/APIRoutes.ts
+++ src/routes/APIRoutes.ts
@@ -6,17 +6,21 @@ const cc = new CoinController();
 
 apiRoutes.get( '/coin/meta', cc.getMeta.bind( cc ) );
 apiRoutes.get( '/coin/modified', cc.lastModified.bind( cc ) );
+apiRoutes.get( '/currency', cc.getCurrency.bind( cc ) );
+apiRoutes.post( '/currency', cc.setCurrency.bind( cc ) );
+
 apiRoutes.get( '/coin/stats', cc.getStats.bind( cc ) );
 apiRoutes.get( '/coin/value', cc.getValue.bind( cc ) );
+
 apiRoutes.get( '/coin/export', cc.export.bind( cc ) );
+apiRoutes.delete( '/coin/reset', cc.reset.bind( cc ) );
+
 apiRoutes.get( '/coin/all', cc.getAllCoins.bind( cc ) );
 apiRoutes.get( '/coin/search', cc.searchCatalog.bind( cc ) );
-apiRoutes.get( '/coin/:id/get', cc.getCoin.bind( cc ) );
 
-apiRoutes.post( '/coin/add', cc.createCoin.bind( cc ) );
+apiRoutes.get( '/coin/:id/get', cc.getCoin.bind( cc ) );
 apiRoutes.put( '/coin/:id/update', cc.updateCoin.bind( cc ) );
 apiRoutes.delete( '/coin/:id/delete', cc.deleteCoin.bind( cc ) );
-
-apiRoutes.delete( '/coin/reset', cc.reset.bind( cc ) );
+apiRoutes.post( '/coin/add', cc.createCoin.bind( cc ) );
 
 export { apiRoutes };
