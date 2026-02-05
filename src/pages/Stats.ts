83743581798646eb51552e321855eb9db0c0d344
komed3 <pk97@outlook.de>
2026-02-05 16:31:21 +0100

    expose coin stats / collection value

--- src/pages/Stats.ts
+++ src/pages/Stats.ts
@@ -1,5 +1,10 @@
 import type { Request, Response } from 'express';
+import DB from '../services/DatabaseService';
 
 export const stats = async ( req: Request, res: Response ) : Promise< void > => {
-    res.render( 'stats', { title: req.t( 'stats.title' ) } );
+    res.render( 'stats', {
+        title: req.t( 'stats.title' ),
+        stats: await DB.getStats(),
+        value: await DB.getValue()
+    } );
 };
