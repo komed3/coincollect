76f0093705443e14b0dac13bd7ae33b0da20d8ba
komed3 <pk97@outlook.de>
2026-02-05 23:12:53 +0100

    set locale (lang)

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,1 +1,6 @@
-document.addEventListener( 'DOMContentLoaded', function () {} );
+document.addEventListener( 'DOMContentLoaded', function () {
+    document.querySelector( '#languageSelect' ).addEventListener( 'change', ( e ) => {
+        document.cookie = `locale=${e.target.value}; path=/; max-age=${ 60 * 60 * 24 * 365 }`;
+        setTimeout( () => window.location.reload(), 250 );
+    } );
+} );
