673e09d39c83f9ea2fd9fd1f7895fc17863c6b77
komed3 <pk97@outlook.de>
2026-02-06 08:32:37 +0100

    get coin data for editing

--- src/pages/Editor.ts
+++ src/pages/Editor.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from 'express';
+import DB from '../services/DatabaseService';
 
 export const editor = async ( req: Request, res: Response ) : Promise< void > => {
-    res.render( 'editor', { title: req.t( 'editor.title' ) } );
+    const coin = req.params.id ? await DB.getCoinById( req.params.id as string ) : undefined;
+    res.render( 'editor', { title: req.t( 'editor.title' ), mode: coin ? 'edit' : 'add', coin } );
 };
