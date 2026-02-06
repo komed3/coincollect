838cb38576596c92b822c8f425978c046510902a
komed3 <pk97@outlook.de>
2026-02-07 00:06:28 +0100

    set up multer disk storage

--- src/middlewares/UploadImages.ts
+++ src/middlewares/UploadImages.ts
@@ -3,3 +3,11 @@ import multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
 
 const uploadsDir = join( process.cwd(), 'uploads' );
+
+const storage = multer.diskStorage( {
+    destination: uploadsDir,
+    filename: ( _req, file, cb ) => {
+        const ext = extname( file.originalname ) || `.${ file.mimetype.split( '/' )[ 1 ] || 'bin' }`;
+        cb( null, `${ uuidv4() }${ext}` );
+    }
+} );
