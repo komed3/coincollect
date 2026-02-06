934991f5ee9a815ac3129ce6b5ca140dae0d0e81
komed3 <pk97@outlook.de>
2026-02-06 09:21:08 +0100

    expose enums

--- src/pages/Editor.ts
+++ src/pages/Editor.ts
@@ -1,7 +1,17 @@
 import type { Request, Response } from 'express';
 import DB from '../services/DatabaseService';
+import { CoinGrade, CoinShape, CoinStatus, CoinType } from '../types';
 
 export const editor = async ( req: Request, res: Response ) : Promise< void > => {
     const coin = req.params.id ? await DB.getCoinById( req.params.id as string ) : undefined;
-    res.render( 'editor', { title: req.t( 'editor.title' ), mode: coin ? 'edit' : 'add', coin } );
+    res.render( 'editor', {
+        title: req.t( 'editor.title' ),
+        mode: coin ? 'edit' : 'add', coin,
+        selectors: {
+            type: Object.values( CoinType ),
+            grade: Object.values( CoinGrade ),
+            status: Object.values( CoinStatus ),
+            shape: Object.values( CoinShape )
+        }
+    } );
 };
