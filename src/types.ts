19e58de4e9166e0d09faf66d49ce7d38f019f5e6
komed3 <pk97@outlook.de>
2026-02-12 23:05:22 +0100

    add enum CoinType

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,21 @@
+export interface CoinBase {}
+
+export interface SingleCoin {}
+
+export enum CoinType {
+    Circulation = 'circulation',
+    SpecialCirculation = 'special',
+    Commemorative = 'commemorative',
+    Bullion = 'bullion',
+    Banknote = 'banknote',
+    Medal = 'medal',
+    Temporary = 'temporary',
+    Pattern = 'pattern',
+    Ingot = 'ingot',
+    Stamp = 'stamp',
+    Other = 'other'
+}
+
 export enum CoinGrade {
     G = 'G',
     VG = 'VG',
