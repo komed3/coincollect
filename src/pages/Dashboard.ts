c6b4734f004e0082b147fb3f504178e12a530ede
komed3 <pk97@outlook.de>
2026-02-13 15:30:21 +0100

    min fix

--- src/pages/Dashboard.ts
+++ src/pages/Dashboard.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from 'express';
 
 export const dashboard = async ( req: Request, res: Response ) : Promise< void > => {
-    res.render( 'home', { title: req.t( 'home.title' ) } );
+    res.render( 'dashboard', { title: req.t( 'dashboard.title' ) } );
 };
