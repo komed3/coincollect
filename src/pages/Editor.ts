ce8617df43d812eebced954709e5cda013654257
komed3 <pk97@outlook.de>
2026-02-13 20:10:44 +0100

    add more editor fields

--- src/pages/Editor.ts
+++ src/pages/Editor.ts
@@ -1,11 +1,12 @@
 import type { Request, Response } from 'express';
-import { CoinShape, CoinType } from '../types';
+import { CoinMaterial, CoinShape, CoinType } from '../types';
 
 export const baseEditor = async ( req: Request, res: Response ) : Promise< void > => {
     res.render( 'editor/base', {
         title: req.t( 'editor.base.title' ),
         keys: {
             type: CoinType,
+            material: CoinMaterial,
             shape: CoinShape
         }
     } );
