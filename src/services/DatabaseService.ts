e705097edbb389e5181cdad6287cc5cf1317b5b3
komed3 <pk97@outlook.de>
2026-02-13 09:17:01 +0100

    add privat db var

--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -1,6 +1,12 @@
+import { Low } from 'lowdb';
+import type { Database } from '../types';
+
 export class DatabaseService {
 
     private static instance: DatabaseService;
+    private db!: Low< Database >;
+
+    private constructor () {}
 
     public static getInstance () : DatabaseService {
         return DatabaseService.instance ||= new DatabaseService();
