778fc74e54982e1452036dd9a312afc8610de3be
komed3 <pk97@outlook.de>
2026-02-13 15:18:45 +0100

    set up express app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,19 @@
+import { join } from 'node:path';
+
 import express from 'express';
 
+
+// express app
+const cwd = process.cwd();
 const app = express();
+
+// view engine
+app.set( 'views', join( cwd, 'views' ) );
+app.set( 'view engine', 'pug' );
+
+// middleware
+app.use( express.json() );
+app.use( express.urlencoded( { extended: true } ) );
+
+// listen ...
 app.listen( 3001 );
