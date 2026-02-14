6b2f2f85c9edf5d8d55072f08735ba416f308bce
komed3 <pk97@outlook.de>
2026-02-14 01:51:11 +0100

    parse coin data

--- public/js/editor/coinBase.js
+++ public/js/editor/coinBase.js
@@ -60,7 +60,42 @@ document.addEventListener( 'DOMContentLoaded', () => {
     form.addEventListener( 'submit', async ( e ) => {
         e.preventDefault();
 
-        // ...
+        const fd = new FormData( form );
+
+        const coinData = {
+            name: val( fd.get( 'name' ), 'string' ),
+            description: val( fd.get( 'description' ), 'string' ),
+            notes: val( fd.get( 'notes' ), 'string' ),
+            type: val( fd.get( 'type' ), 'string' ),
+            country: val( fd.get( 'country' ), 'string' ),
+            series: val( fd.get( 'series' ), 'string' ),
+            tags: val( fd.get( 'tags' ), 'list' ) ?? [],
+            currency: val( fd.get( 'currency' ), 'string' ),
+            nominal: {
+                value: val( fd.get( 'nominalValue' ), 'string' ),
+                unit: val( fd.get( 'nominalUnit' ), 'string' )
+            },
+            issuer: val( fd.get( 'issuer' ), 'string' ),
+            issueDate: val( fd.get( 'issueDate' ), 'date' ),
+            devaluationDate: val( fd.get( 'devaluationDate' ), 'date' ),
+            mintStartYear: val( fd.get( 'mintStartYear' ), 'number' ),
+            mintEndYear: val( fd.get( 'mintEndYear' ), 'number' ),
+            mintMarks: val( fd.get( 'mintMarks' ), 'list' ),
+            design: {
+                shape: val( fd.get( 'shape' ), 'string' ),
+                obverse: val( fd.get( 'obverse' ), 'string' ),
+                reverse: val( fd.get( 'reverse' ), 'string' ),
+                edge: val( fd.get( 'edge' ), 'string' )
+            },
+            // material
+            dimension: {
+                diameter: val( fd.get( 'diameter' ), 'number' ),
+                thickness: val( fd.get( 'thickness' ), 'number' ),
+                weight: val( fd.get( 'weight' ), 'number' )
+            },
+            // images
+            // identifiers
+        };
     } );
 
     // reset form
