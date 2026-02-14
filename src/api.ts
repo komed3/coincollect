c8f7063955b25b136ec7f8e68ec6306b2d9a8aff
komed3 <pk97@outlook.de>
2026-02-14 09:55:39 +0100

    create coin service

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import { CoinService } from './services/CoinService';
 
 const api = Router();
+const service = new CoinService();
+
+api.post( '/base/add', service.addCoinBase.bind( service ) );
 
 export { api };
--- src/services/CoinService.ts
+++ src/services/CoinService.ts
@@ -1,3 +1,19 @@
 import type { Request, Response } from 'express';
+import DB from './DatabaseService';
 
-export const addCoinBase = async ( req: Request, res: Response ) : Promise< void > => {};
+export class CoinService {
+
+    private readonly dbService = DB;
+
+    private async catch ( res: Response, msg: string, fn: () => Promise< void > ) : Promise< void > {
+        try { await fn() } catch ( err ) { res.status( 500 ).json( { msg, err } ) }
+    }
+
+    public async addCoinBase ( req: Request, res: Response ) : Promise< void > {
+        await this.catch( res, 'Failed to add coin base', async () => {
+            const coin = await this.dbService.addCoinBase( req.body );
+            res.status( 201 ).json( coin );
+        } );
+    }
+
+}
