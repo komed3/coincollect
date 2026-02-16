458ae2912e12a878db9ac29edcb9b14cb585e56e
komed3 <pk97@outlook.de>
2026-02-16 22:43:38 +0100

    Create Stats.ts

---
+++ src/pages/Stats.ts
@@ -0,0 +1,10 @@
+import type { Request, Response } from 'express';
+import DB from '../services/DatabaseService';
+
+export const stats = async ( req: Request, res: Response ) : Promise< void > => {
+    res.render( 'stats', {
+        page: 'stats',
+        title: req.t( 'stats.title' ),
+        stats: DB.getStats()
+    } );
+};
