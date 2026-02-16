a99a5b64f29c387415339e2169dd0d5d7c404699
komed3 <pk97@outlook.de>
2026-02-16 22:44:44 +0100

    add stats route

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { dashboard } from './pages/Dashboard';
 import { baseEditor, coinEditor } from './pages/Editor';
+import { stats } from './pages/Stats';
 import { baseView, coinView } from './pages/View';
 
 const routes = Router();
 routes.get( '{/}', dashboard );
+routes.get( '/stats{/}', stats );
 
 routes.get( '/base/:id{/}', baseView );
 routes.get( '/coin/:id{/}', coinView );
