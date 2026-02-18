c1c3df781e38645703aaac05ed6403fffb821c85
komed3 <pk97@outlook.de>
2026-02-18 23:46:25 +0100

    load filter

--- public/js/list.js
+++ public/js/list.js
@@ -1,6 +1,11 @@
 class CCList {
 
     constructor () {
+        this.tableContent = document.querySelector( '.cc-list--table-content' );
+        this.emptyState = document.querySelector( '.cc-list--table-empty' );
+        this.searchInput = document.querySelector( '.cc-list--filter-search-input' );
+        this.filterSelects = document.querySelectorAll( '.cc-list--filter-select' );
+
         this.initializeEvents();
         this.init();
     }
@@ -11,7 +16,24 @@ class CCList {
 
     initializeEvents () {}
 
-    async loadFilter () {}
+    async loadFilter () {
+        try {
+            const res = await fetch( '/api/stats' );
+            if ( ! res.ok ) return;
+            const stats = await res.json();
+
+            const selects = Array.from( this.filterSelects );
+            this.populateSelectFromKeys( selects[ 0 ], stats.type, I18N.type );
+            this.populateSelectFromKeys( selects[ 1 ], stats.status, I18N.status );
+            this.populateSelectFromKeys( selects[ 2 ], stats.grade, I18N.grade );
+            this.populateSelectFromKeys( selects[ 3 ], stats.country );
+            this.populateSelectFromKeys( selects[ 4 ], stats.currency );
+            this.populateSelectFromKeys( selects[ 5 ], stats.year );
+            this.populateSelectFromKeys( selects[ 6 ], stats.material, I18N.material );
+
+            this.fromHash();
+        } catch { /** silent */ }
+    }
 
 }
 
